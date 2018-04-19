a8f69cac0f4bd0af305881274b1cbeb62d97b8fd
amosbastian <amos_bastian@hotmail.com>
2018-04-19 22:38:24 +0200

    Remove everything that is going to be remade

--- utopian/utopian_client.py
+++ utopian/utopian_client.py
@@ -3,21 +3,17 @@ import json
 import math
 import os
 import requests
-import threading
-from multiprocessing import Pool
-from functools import partial
 from dateutil.parser import parse
 from pymongo import MongoClient
-from steem import Steem
 
-try:
-    from urllib import urlencode
-except ImportError:
-    from urllib.parse import urlencode
+# try:
+#     from urllib import urlencode
+# except ImportError:
+from urllib.parse import urlencode
 
 CLIENT = MongoClient()
 DB = CLIENT.utopian
-
+ERROR = "{ERROR}"
 UTOPIAN_API = "https://api.utopian.io/api/"
 
 HEADERS = {
@@ -64,7 +60,7 @@ def create_post(post, status, update=True):
         # Add post's score
         if "score" in post["json_metadata"].keys():
             new_post["score"] = post["json_metadata"]["score"]
-            if new_post["score"] == None:
+            if new_post["score"] is None:
                 new_post["score"] = 0
         else:
             new_post["score"] = 100
@@ -92,66 +88,65 @@ def get_posts(status, update=True):
     else:
         r = requests.get(generate_url(action, {"filterBy": "review",
                          "limit": 1}), headers=HEADERS)
+
     if r.status_code == 200:
         total = r.json()["total"]
         total = math.ceil(total / 1000)
     else:
         time = datetime.datetime.now()
-        print(f"{time} - Something went wrong, please try again later.")
+        print(f"{time} - {ERROR}")
         return
 
     # Get ALL posts submitted to Utopian.io
     if not update:
         for _ in range(total):
+            # Parameters depend on status of the post
             if not status == "pending":
                 parameters = {"status": status, "limit": limit, "skip": skip}
             else:
-                parameters = {"filterBy": "review", "limit": limit, "skip": skip}
+                parameters = {"filterBy": "review", "limit": limit,
+                              "skip": skip}
+
+            # Generate URL and send the request to the API
             url = generate_url(action, parameters)
             print(f"{datetime.datetime.now()} - Fetching from {url}")
             r = requests.get(url, headers=HEADERS)
+
+            # If request was okay then replace posts in the database
             if r.status_code == 200:
-                pool = Pool()
-                x = partial(create_post, status=status, update=False)
-                post_list = pool.map(x, r.json()["results"])
-                pool.close()
-                pool.join()
+                post_list = [create_post(post, status, False)
+                             for post in r.json()["results"]]
+
                 for post in post_list:
-                    if not post == None:
+                    if post is not None:
                         posts.replace_one({"_id": post["_id"]}, post, True)
             else:
                 time = datetime.datetime.now()
-                print(f"{time} - Something went wrong, please try again later.")
+                print(f"{time} - {ERROR}")
                 return
             skip += 1000
     # Get posts submitted to Utopian.io within the last week
     else:
         week = datetime.datetime.now() - datetime.timedelta(days=7)
         for _ in range(total):
+            # Set parameters and send a request to the API
             parameters = {"status": status, "limit": limit, "skip": skip}
             url = generate_url(action, parameters)
             print(f"{datetime.datetime.now()} - Fetching from {url}")
             r = requests.get(url, headers=HEADERS)
+
+            # If request was okay then replace posts in the database
             if r.status_code == 200:
-                pool = Pool()
-                x = partial(create_post, status=status, update=True)
-                post_list = pool.map(x, r.json()["results"])
-                pool.close()
-                pool.join()
+                post_list = [create_post(post, status, True)
+                             for post in r.json()["results"]]
                 for post in post_list:
-                    if post is None:
-                        return
-                    database_post = posts.find_one({"_id": post["_id"]})
-                    if database_post and "flagged" in database_post:
-                        if (not database_post["flagged"] == post["flagged"]
-                            or database_post["modified"]):
-                            post["modified"] = True
-                    posts.replace_one({"_id": post["_id"]}, post, True)
-                    if post["created"] < week:
-                        return
+                    if post is not None:
+                        if post["created"] > week:
+                            return
+                        posts.replace_one({"_id": post["_id"]}, post, True)
             else:
                 time = datetime.datetime.now()
-                print(f"{time} - Something went wrong, please try again later.")
+                print(f"{time} - {ERROR}")
                 return
 
             skip += 1000
@@ -161,9 +156,11 @@ def get_moderators():
     action = "moderators"
     url = generate_url(action, {})
     r = requests.get(url, headers=HEADERS)
+
+    
     if r.status_code == 200:
         return r.json()["results"]
     else:
         time = datetime.datetime.now()
-        print(f"{time} - Something went wrong, please try again later.")
+        print(f"{time} - {ERROR}")
         return
