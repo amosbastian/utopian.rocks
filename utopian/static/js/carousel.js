52ab71c05fa501e469819cb2ec3566ab481d13ba
amosbastian <amos_bastian@hotmail.com>
2018-04-23 02:17:55 +0200

    Finish most styling, media queries to categories

---
+++ utopian/static/js/carousel.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+  $('.home-managers__slick').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '5rem',
+    infinite: true,
+    dots: true,
+    accessibility: false,
+    responsive: [
+      {
+        breakpoint: 1350,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '10rem'
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '5rem'
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function(){
+  $('.home-projects__slick').slick({
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    centerMode: true,
+    centerPadding: '10rem',
+    infinite: true,
+    dots: true,
+    accessibility: false,
+    responsive: [
+      {
+        breakpoint: 1350,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '20rem'
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '15rem'
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '10rem'
+        }
+      }
+    ]
+  });
+});
