08b9bfdd7955fd7576af6ede532effa6be0082bf
amosbastian <amos_bastian@hotmail.com>
2018-05-22 23:23:08 +0200

    Add endpoint for contributions + begin of weekly statistics

--- utopian/app.py
+++ utopian/app.py
@@ -1,10 +1,13 @@
-import datetime
 import json
 import os
-from bson.json_util import dumps
+from bson import json_util
+from datetime import datetime, timedelta
+from dateutil.parser import parse
 from flask import Flask, jsonify, render_template
 from flask_restful import Resource, Api
 from pymongo import MongoClient
+from webargs import fields, validate
+from webargs.flaskparser import use_args, use_kwargs, parser, abort
 
 CLIENT = MongoClient()
 DB = CLIENT.utempian
@@ -36,13 +39,69 @@ def index():
     return render_template("index.html", contributions=unreviewed)
 
 
-class Test(Resource):
-    def get(self, status):
-        contributions = [dumps(c) for c in DB.contributions.find()]
-        print(contributions[0])
-        return contributions
+def without_score(contribution):
+    """
+    Returns a contribution without the score.
+    """
+    return {x: contribution[x] for x in contribution if x != "score"}
 
-api.add_resource(Test, "/contributions/<string:status>")
+
+class ContributionResource(Resource):
+    """
+    Endpoint for contributions in the spreadsheet.
+    """
+    query_parameters = {
+        "category": fields.Str(),
+        "status": fields.Str(),
+        "author": fields.Str(),
+        "moderator": fields.Str(),
+        "staff_picked": fields.Bool()
+    }
+
+    @use_args(query_parameters)
+    def get(self, query_parameters):
+        """
+        Uses the given query parameters to search for contributions in the
+        database.
+        """
+        contributions = [json.loads(json_util.dumps(without_score(c)))
+                         for c in DB.contributions.find(query_parameters)]
+        return jsonify(contributions)
+
+
+def string_to_date(input):
+    """
+    Converts a given string to a date.
+    """
+    if input == "today":
+        return datetime.now()
+    try:
+        date = parse(input)
+        return date
+    except Exception as error:
+        abort(422, errors=str(error))
+
+
+class WeeklyResource(Resource):
+    """
+    Endpoint for weekly contribution data (requested).
+    """
+    def get(self, date):
+        # Get date for retrieving posts
+        date = string_to_date(date)
+        week_ago = date - timedelta(days=7)
+
+        # Retrieve contributions made in week before the given date
+        contributions = DB.contributions
+        pipeline = [{"$match": {"review_date": {"$gt": week_ago}}}]
+        contribution_weekly = [json.loads(json_util.dumps(c))
+                               for c in contributions.aggregate(pipeline)]
+
+        return jsonify(contribution_weekly)
+
+
+api.add_resource(WeeklyResource, "/api/weekly/<string:date>")
+api.add_resource(ContributionResource, "/api/posts")
 
 
 def main():
