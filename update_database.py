c2528448ac109738cce8e36d39204b3e71e07713
amosbastian <amosbastian@gmail.com>
2018-08-14 13:40:10 +0100

    fix: TypeError when recharge_timedelta = 0

--- update_database.py
+++ update_database.py
@@ -149,12 +149,15 @@ def update_account():
     recharge_time = account.get_recharge_time_str(99.75)
     recharge_timedelta = account.get_recharge_timedelta(99.75)
 
-    if recharge_timedelta > timedelta(hours=1):
-        recharge_class = "recharge--high"
-    elif (recharge_timedelta < timedelta(hours=1) and
-          recharge_timedelta > timedelta(minutes=30)):
-        recharge_class = "recharge--average"
-    else:
+    try:
+        if recharge_timedelta > timedelta(hours=1):
+            recharge_class = "recharge--high"
+        elif (recharge_timedelta < timedelta(hours=1) and
+            recharge_timedelta > timedelta(minutes=30)):
+            recharge_class = "recharge--average"
+        else:
+            recharge_class = "recharge--low"
+    except TypeError:
         recharge_class = "recharge--low"
 
     accounts = constants.DB.accounts
