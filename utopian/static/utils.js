85be8e6ea07123eb69e7e42fcc190457137f8cd7
amosbastian <amosbastian@gmail.com>
2018-11-02 18:41:09 +0000

    Update recharge timer and voting power display

--- utopian/static/utils.js
+++ utopian/static/utils.js
@@ -1,61 +1,65 @@
-var seconds;
-var temp;
+let clockTimer;
+let votingPowerTimer;
 
-function countdown() {
-  time = document.getElementById('time').innerHTML;
-  timeArray = time.split(':')
-  seconds = timeToSeconds(timeArray);
-  if (seconds == '') {
-    temp = document.getElementById('time');
-    temp.innerHTML = "00:00:00";
-    return;
-  }
-  seconds--;
-  temp = document.getElementById('time');
-  temp.innerHTML = secondsToTime(seconds);
-  wait = setTimeout(countdown, 1000);
+function displayTimeLeft(seconds) {
+  const timerDisplay = document.getElementById('time');
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor(seconds % 3600 / 60);
+  const remainderSeconds = Math.floor(seconds % 3600 % 60);
+  const display = `${hours}:${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
 }
 
-function timeToSeconds(timeArray) {
-  var minutes = (timeArray[0] * 60) + (timeArray[1] * 1);
-  var seconds = (minutes * 60) + (timeArray[2] * 1);
-  return seconds;
+function rechargeTimer() {
+  const rechargeTime = document.getElementById('time').innerHTML;
+  const timeArray = rechargeTime.split(':').map(number => parseInt(number, 10));
+
+  const [hours, minutes, seconds] = timeArray;
+  const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+  displayTimeLeft(totalSeconds);
+
+  const now = Date.now();
+  const then = now + totalSeconds * 1000;
+
+  clockTimer = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(clockTimer);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
 }
 
-function secondsToTime(secs) {
-  var hours = Math.floor(secs / (60 * 60));
-  hours = hours < 10 ? '0' + hours : hours;
-  var divisor_for_minutes = secs % (60 * 60);
-  var minutes = Math.floor(divisor_for_minutes / 60);
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  var divisor_for_seconds = divisor_for_minutes % 60;
-  var seconds = Math.ceil(divisor_for_seconds);
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-  return hours + ':' + minutes + ':' + seconds;
+function displayVotingPower(currentVotingPower) {
+  const votingPowerDisplay = document.getElementById('current-vp');
+  votingPowerDisplay.textContent = currentVotingPower.toFixed(2);
 }
 
 function votingPower() {
-  let vp = parseFloat(document.getElementById('current-vp').innerHTML.replace("%", ""));
-  vp += 0.01
-  if (vp > 100) {
-    vp = 100.0;
-  }
-  let temp = document.getElementById('current-vp');
-  temp.innerHTML = vp.toFixed(2);
-  var wait = setTimeout(votingPower, 43200);
+  let currentVotingPower = parseFloat(document.getElementById('current-vp').innerHTML.replace('%', ''));
+
+  votingPowerTimer = setInterval(() => {
+    currentVotingPower += 0.01;
+    if (currentVotingPower > 100.0) {
+      clearInterval(votingPowerTimer);
+      return;
+    }
+    displayVotingPower(currentVotingPower);
+  }, 43200);
 }
 
 const contributions = document.getElementsByClassName('contribution');
-
 let filterCategories = [];
 let filterButtons;
 let allButton;
 
-document.addEventListener("DOMContentLoaded", () => {
-  filterButtons = document.querySelectorAll(".filter-button");
+document.addEventListener('DOMContentLoaded', () => {
+  filterButtons = document.querySelectorAll('.filter-button');
   allButton = document.querySelector('.category--all');
 
-  filterButtons.forEach(element => {
+  filterButtons.forEach((element) => {
     const buttonCategory = element.classList[1].replace('category--', '');
     element.addEventListener('click', () => {
       if (buttonCategory != 'all') {
@@ -66,22 +70,15 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
-function updateCounter() {
-  contributionCounter = document.querySelector('.contribution-counter');
-  numberContributions = document.querySelectorAll('.show').length;
-  contributionCounter.innerHTML = numberContributions;
-}
-
 /* Shows all contributions and turn all category buttons to inactive */
 function showContributions() {
-  for (let contribution of contributions) {
+  for (const contribution of contributions) {
     contribution.classList.add('show');
   }
 
-  filterButtons.forEach(element => {
+  filterButtons.forEach((element) => {
     const buttonCategory = element.classList[1].replace('category--', '');
-    if (buttonCategory != 'all') {
+    if (buttonCategory !== 'all') {
       element.classList.add('category--inactive');
     } else {
       element.classList.remove('category--inactive');
@@ -89,7 +86,14 @@ function showContributions() {
   });
 }
 
-/* Show all contributions if category === 'all', else toggle category's 
+
+function updateCounter() {
+  const contributionCounter = document.querySelector('.contribution-counter');
+  const numberContributions = document.querySelectorAll('.show').length;
+  contributionCounter.innerHTML = numberContributions;
+}
+
+/* Show all contributions if category === 'all', else toggle category's
    contributions */
 function filterContributions(category) {
   if (category === 'all') {
@@ -97,19 +101,17 @@ function filterContributions(category) {
     showContributions();
     updateCounter();
     return;
-  } 
+  }
 
   allButton.classList.add('category--inactive');
 
   if (filterCategories.includes(category)) {
-    filterCategories = filterCategories.filter(element => {
-      return element !== category;
-    });
+    filterCategories = filterCategories.filter(element => element !== category);
   } else {
     filterCategories.push(category);
   }
 
-  for (let contribution of contributions) {
+  for (const contribution of contributions) {
     contribution.classList.remove('show');
     const contributionCategory = contribution.classList[1];
     if (filterCategories.includes(contributionCategory)) {
